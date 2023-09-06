e4add70dc8b63762d1e6e9bf9a80d425b5e5c733
Vsevolod Agafonov <vsevolod.agafonov@yandex.ru>
2023-09-06 23:18:48 +0300

    feat: filter

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,39 @@
 <template class="bg-black">
-    <h1>Lines {{ info.count }}</h1>
-    <h1>Pages {{ info.pages }}</h1>
-    <form v-on:submit.prevent="formSubmit">
-      <input type="text" v-model="nameInput"
-    class="border-black border-2 rounded-md" placeholder="Find character">
-      <button type="submit">Filter</button>
-    </form>
-    
-    <div class="flex gap-5 flex-col items-center">
-      <div class="p-4 border-purple-900 bg-gray-400 rounded-md border-2 flex flex-col justify-center items-center"
-        v-for="item in characters" :key="item.id">
-        <p>Key: {{ item.id }}</p>
-        <NuxtLink to="/character">Name: {{ item.name }}</NuxtLink>
-        <img class="w-1/3 rounded-md" :src=item.image>
-        <p>Status: {{ item.status }}</p>
-        <p id="posts" v-for="episode in item.episode.slice(0, 5)">Episodes: {{ episode }}</p>
-      </div>
+  <h1>Lines {{ info.count }}</h1>
+  <h1>Pages {{ info.pages }}</h1>
+  <form v-on:submit.prevent="formSubmit">
+    <input type="text" v-model="nameInput" class="border-black border-2 rounded-md" placeholder="Find character">
+    <button type="submit">Filter</button>
+  </form>
+  <hr />
+  <div class="flex gap-2">
+    <input type="radio" id="alive" value="alive" 
+    v-model="selectedOption" @change="(e) => handleStatusChange(e)" />
+    <label for="option1">Alive</label>
+
+    <input type="radio" id="dead" value="dead" 
+    v-model="selectedOption" @change="(e) => handleStatusChange(e)" />
+    <label for="option2">Dead</label>
+
+    <input type="radio" id="unknown" value="unknown" 
+    v-model="selectedOption" @change="(e) => handleStatusChange(e)" />
+    <label for="option3">Unknown</label>
+  </div>
+
+  <div class="flex gap-5 flex-col items-center">
+    <div class="p-4 border-purple-900 bg-gray-400 rounded-md border-2 flex flex-col justify-center items-center"
+      v-for="item in characters" :key="item.id">
+      <p>Key: {{ item.id }}</p>
+      <NuxtLink to="/character">Name: {{ item.name }}</NuxtLink>
+      <img class="w-1/3 rounded-md" :src=item.image>
+      <p>Status: {{ item.status }}</p>
+      <p id="posts" v-for="episode in item.episode.slice(0, 5)">Episodes: {{ episode }}</p>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
-import { Character, Info } from '~/types';
 import { useCharacterStore } from '~/store/character';
 
 const characterStore = useCharacterStore();
@@ -30,20 +42,15 @@ const { characters, info } = storeToRefs(characterStore);
 
 characterStore.getCharacters();
 let nameInput = '';
+let selectedOption = '';
 
-// export type Response = {
-//   info: { count: string; pages: string; next: string; prev: string };
-//   results: Array<Character>;
-// };
-
-// function inputChange(event: FormDataEvent) {
-//   this.nameInput = event.target.value
-// }
-// const info = (data.value as any).info;
-// const results: Character[] = (data.value as any).results;
+function handleStatusChange (e: any) {
+  selectedOption = e.target.value;
+  characterStore.filterCharacters(nameInput, selectedOption)
+}
 
 function formSubmit() {
-  characterStore.filterCharacters(nameInput)
+  characterStore.filterCharacters(nameInput, selectedOption)
 }
 </script>
 
--- store/character.ts
+++ store/character.ts
@@ -6,29 +6,51 @@ export const useCharacterStore = defineStore({
     return {
       characters: [] as Character[],
       info: [] as Info[],
+      selectenName: '',
+      selectedStatus: '',
     }
   },
 
   actions: {
     async getCharacters() {
-      const { data } = await useFetch("https://rickandmortyapi.com/api/character") as {data: any};
+      const { data } = await useFetch("https://rickandmortyapi.com/api/character") as { data: any };
       this.characters = data.value.results;
       this.info = data.value.info;
-      },
-      async filterCharacters(name: string) {
-        const { data } = await useFetch(`https://rickandmortyapi.com/api/character/?name=${name}`) as {data: any};
+    },
+    async filterCharacters(name: string, status: string) {
+      if (name && status) {
+        const { data } = await useFetch(`https://rickandmortyapi.com/api/character/?name=${name}&status=${status}`) as { data: any };
         this.characters = data.value.results;
         this.info = data.value.info;
-        }
+      }
+      if (name && !status) {
+        const { data } = await useFetch(`https://rickandmortyapi.com/api/character/?name=${name}`) as { data: any };
+        this.characters = data.value.results;
+        this.info = data.value.info;
+      }
+      if (!name && status) {
+        const { data } = await useFetch(`https://rickandmortyapi.com/api/character/?status=${status}`) as { data: any };
+        this.characters = data.value.results;
+        this.info = data.value.info;
+      }
+    },
+
+    // async filterAlive(status: string) {
+    //   const { data } = await useFetch(`https://rickandmortyapi.com/api/character/?status=${status}`) as { data: any };
+    //   this.characters = data.value.results;
+    //   this.info = data.value.info;
+    // }
   },
 
   persist: {
-    storage: persistedState.cookiesWithOptions({
-      sameSite: 'strict',
-    }),
+    storage: persistedState.localStorage,
   },
+
+  // persist: {
+  //   storage: persistedState.cookiesWithOptions({
+  //     sameSite: 'strict',
+  //   }),
+  // },
 })
 
-// persist: {
-//   storage: persistedState.localStorage,
-// },
+
